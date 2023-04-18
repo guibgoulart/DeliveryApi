41b81432e961b9175ec37240c87af633e87e896b
guilherme.barreto <g.goulart97@edu.pucrs.br>
2023-04-18 11:22:20 -0300

    add swagger doc for rest of controllers

--- src/main/java/com/deliveryapi/controller/DeliveryController.java
+++ src/main/java/com/deliveryapi/controller/DeliveryController.java
@@ -6,6 +6,10 @@ import com.deliveryapi.application.DeliveryFinishService;
 import com.deliveryapi.application.DeliveryRequestService;
 import com.deliveryapi.domain.delivery.DeliveryResponse;
 import com.deliveryapi.domain.delivery.DeliveryMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,31 +20,57 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/delivery")
+@Api(tags = "Deliveries")
 public class DeliveryController {
 
     private final DeliveryRequestService deliveryRequestService;
     private final DeliveryFinishService deliveryFinishService;
     private final DeliveryMapper mapper = DeliveryMapper.INSTANCE;
 
+    @ApiOperation("Requests a new delivery")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Delivery requested"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DeliveryResponse requestDelivery(@Valid @RequestBody DeliveryRequest deliveryRequest) {
 
-         Delivery requestDelivery = deliveryRequestService.request(mapper.deliveryRequestToDelivery(deliveryRequest));
+        Delivery requestDelivery = deliveryRequestService.request(mapper.deliveryRequestToDelivery(deliveryRequest));
 
-         return mapper.deliveryToDeliveryResponse(requestDelivery);
+        return mapper.deliveryToDeliveryResponse(requestDelivery);
     }
 
+    @ApiOperation("Returns all deliveries")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "List of deliveries returned"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @GetMapping
     public List<DeliveryResponse> findAll() {
         return mapper.deliveryListToDeliveryResponseList(deliveryRequestService.findAll());
     }
 
+    @ApiOperation("Returns a delivery by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Delivery returned"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Delivery not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @GetMapping("/{deliveryId}")
     public DeliveryResponse findById(@PathVariable Long deliveryId) {
         return mapper.deliveryToDeliveryResponse(deliveryRequestService.findById(deliveryId));
     }
 
+    @ApiOperation("Finishes a delivery")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Delivery finished"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Delivery not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @PutMapping("/{deliveryId}/finish")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void finish(@PathVariable Long deliveryId) {
--- src/main/java/com/deliveryapi/controller/EventController.java
+++ src/main/java/com/deliveryapi/controller/EventController.java
@@ -6,6 +6,10 @@ import com.deliveryapi.domain.delivery.Delivery;
 import com.deliveryapi.application.DeliveryRequestService;
 import com.deliveryapi.application.EventRegisterService;
 import com.deliveryapi.domain.event.EventMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +19,17 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/delivery/{deliveryId}/event")
+@Api(tags = {"Event"})
 public class EventController {
 
     private final DeliveryRequestService deliveryRequestService;
     private final EventRegisterService eventRegisterService;
     private final EventMapper mapper;
 
+    @ApiOperation(value = "Register an event for a delivery")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Event registered successfully", response = EventResponse.class),
+            @ApiResponse(code = 400, message = "Invalid event description")})
     @PostMapping
     public EventResponse register(@PathVariable Long deliveryId,
                                   @Valid @RequestBody EventRequest eventRequest) {
@@ -29,6 +38,12 @@ public class EventController {
                 eventRegisterService.register(deliveryId, eventRequest.description()));
     }
 
+    @ApiOperation(value = "List all events for a delivery", notes = "Returns a list of all events for the specified delivery")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list of events"),
+            @ApiResponse(code = 404, message = "Delivery not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @GetMapping
     public List<EventResponse> list(@PathVariable Long deliveryId) {
 
